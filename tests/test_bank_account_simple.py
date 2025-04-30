3a99a6edafc3bbf7641f7fd0de6c559854a7d01b
Fabricio Braga <fabribraga@gmail.com>
2025-04-30 15:50:28 +0100

    task-1234: adding modules and structure change

--- tests/test_bank_account_simple.py
+++ tests/test_bank_account_simple.py
@@ -7,24 +7,25 @@
 # python3 -m unittest discover -s tests
 
 import unittest
-from bank_account_simple import BankAccount
+from bank.simple_account import BankAccount
 
-class TestBankAccount (unittest.TestCase):
+class TestBankAccount(unittest.TestCase):
 
-    def setUp(self): # this is the method that we call everytime we start the test
+    def setUp(self):
         self.account = BankAccount("Alice", 100)
 
     def test_deposit(self):
         self.account.deposit(50)
         self.assertEqual(self.account.balance, 150)
 
-    def test_withdraw_sucess(self):
+    def test_withdraw_success(self):
         self.account.withdraw(50)
         self.assertEqual(self.account.balance, 50)
 
-    def test_withdraw_more_than_we_have(self):
-        with self.assertRaises(ValueError):    
-            self.account.withdraw(1000) 
+    def test_withdraw_more_than_balance(self):
+        with self.assertRaises(ValueError):
+            self.account.withdraw(1000)
 
-    if __name__ == "__main__":
-        unittest.main()
+if __name__ == "__main__":
+    unittest.main()
+
